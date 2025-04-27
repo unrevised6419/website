954090f054ad5548eef06896c76fe6c07483e9d1
Andrei Luca <1881266+unrevised6419@users.noreply.github.com>
2025-04-27 09:46:21 +0300

    chore: upgrade tailwind

--- astro.config.js
+++ astro.config.js
@@ -1,16 +1,20 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+
 import react from "@astrojs/react";
 import inline from "@playform/inline";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://luca.md/",
 	integrations: [
 		react(),
-		tailwind(),
 		inline({
 			Exclude: (file) => !file.endsWith("dist/index.html"),
 		}),
 	],
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
